0c884c4e466246244178ad896fc495a1d7f55d1b
etiiizel <franciscoetizel@gmail.com>
2025-05-05 14:46:10 -0300

    btn-styled_components-ts

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,11 @@
-export function Button() {
-  return <button>Enviar</button>;
+import styles from './Button.module.css';
+
+interface ButtonProps {
+  color?: 'primary' | 'secundary' | 'danger' | 'sucess';
+}
+
+export function Button({ color = 'secundary' }: ButtonProps) {
+  return (
+    <button className={`${styles.button} ${styles[color]}`}>Enviar</button>
+  );
 }
