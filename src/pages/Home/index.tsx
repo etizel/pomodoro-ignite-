78e5d88197f546df92f0c0748518c86badd0b6e7
etiiizel <franciscoetizel@gmail.com>
2025-05-28 17:07:57 -0300

    datalist-task-suggestions

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,10 +13,30 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Irei Trabalhar em:</label>
-          <input id="task" placeholder="Dê um nome ao teu projeto" />
+          <input
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome ao teu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+            <option value="Projeto 1"></option>
+            {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+            <option value="Projeto 2"></option>
+            {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+            <option value="Projeto 3"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" placeholder="00" step={5} />
+          <input
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
